816d587e50d09c6b113447ecbfdf69126ffe4a49
pgenev <philip_genev@abv.bg>
2017-07-11 18:17:53 +0300

    changes

--- flask-introduction/run_app.py
+++ flask-introduction/run_app.py
@@ -2,10 +2,10 @@ import os
 
 # from library._01_simple import app
 # from library._02_html_inside_view import app
-from library._03_template_str_inside_view import app
+# from library._03_template_str_inside_view import app
 # from library._04_template_outside_view import app
 # from library._05_basic_routing import app
-# from library._06_raising_custom_errors import app
+from library._06_raising_custom_errors import app
 # from library._07_request_info import app
 # from library._08_redirects import app
 # from library._09_simple_database_app import app
